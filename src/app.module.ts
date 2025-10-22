ef20ba0c1b0417def0d5262e1151d9159a84453b
arthur-hc <arthurhermannc@gmail.com>
2025-10-22 19:45:31 -0300

    chore: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { mySqlDBConfig } from './config/mySqlDB.config';
 import { TaskManagerModule } from './modules/taskManager/TaskManager.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(mySqlDBConfig), TaskManagerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
