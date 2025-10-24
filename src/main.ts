cb112bd97d4e29e238713131c00996e1eda6e054
arthur-hc <arthurhermannc@gmail.com>
2025-10-23 21:38:44 -0300

    feat: enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Implementar Helmet antes de ir para produção
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
